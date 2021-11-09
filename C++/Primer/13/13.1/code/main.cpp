4f0a99fc838ba1c379bc7cf3fb5db1719423610d
kavinwkp <1817136760@qq.com>
2021-11-09 15:09:48 +0800

    21_11_9

--- C++/Primer/13/13.1/code/main.cpp
+++ C++/Primer/13/13.1/code/main.cpp
@@ -8,26 +8,42 @@ private:
     string name;
     int age;
 public:
-    Person(string name, int age) : name(name), age(age) {}
-    Person(const Person& rhs) {
-        cout << "-----copy constructor-----" << endl;
+    Person(string name, int age) : name(name), age(age)
+    {
+        cout << "constructor" << endl;
+    }
+    Person(const Person& rhs)
+    {
+        cout << "copy constructor" << endl;
         name = rhs.name;
         age = rhs.age;
     }
-    Person& operator=(const Person& rhs) {
-        cout << "-----copy-assignment-----" << endl;
-        name = rhs.name;
-        age = rhs.age;
-        return *this;
-    }
+    // Person& operator=(const Person& rhs) {
+    //     cout << "-----copy-assignment-----" << endl;
+    //     name = rhs.name;
+    //     age = rhs.age;
+    //     return *this;
+    // }
 };
+
+void func(Person p) {
+    cout << "Entering func" << endl;
+    return;
+}
+
+Person func2() {
+    Person *p = new Person("lisa", 34);
+    return *p;
+}
 
 int main(int argc, char const *argv[])
 {
-    Person p1("kavin", 23);
-    Person p2(p1);
-    // Person p3("jack", 12);
-    // p3 = p1;
+    // Person p1("kavin", 23);
+    // Person p2(p1);
+    // Person p3 = p1;
+    // func(p1);
+    // Person *ptr = new Person("kavin", 34);
+    Person p2 = func2();
     return 0;
 }
 
--- C++/Primer/13/13.6/main2.cpp
+++ C++/Primer/13/13.6/main2.cpp
@@ -14,25 +14,37 @@ public:
         cout << "copy constructor" << endl;
     }    // 拷贝构造函数
 
-    // Person(Person &&p) noexcept // 移动构造函数
-    //     : ptr(p.ptr), age(p.age)
-    // {
-    //     cout << "move constructor" << endl;
-    //     p.ptr = nullptr; // 源对象的指针置空
-    // }
+    Person(Person &&p) noexcept // 移动构造函数
+        : ptr(p.ptr), age(p.age)
+    {
+        cout << "move constructor" << endl;
+        p.ptr = nullptr; // 源对象的指针置空
+    }
 
-    // Person& operator=(const Person rhs) {
-    //     swap(*this, rhs);
-    //     return *this;
-    // }
-
+    Person& operator=(Person rhs) {
+        cout << "swap copy assignment" << endl;
+        swap(*this, rhs);
+        return *this;
+    }
+    friend void swap(Person&, Person&);
     ~Person() { delete ptr; }
 };
+
+inline
+void swap(Person& lhs, Person& rhs) {
+    using std::swap;
+    swap(lhs.ptr, rhs.ptr);
+    swap(lhs.age, rhs.age);
+}
 
 int main(int argc, char const *argv[])
 {
     Person p1("kavin"); // 直接初始化
     // Person p2(p1);      // 调用拷贝构造函数直接初始化
     // Person p2 = p1;     // 拷贝初始化，编译器会优化为上面那种形式
+    Person p2;
+    p2 = p1;
+    cout << "----------" << endl;
+    p2 = std::move(p1);
     return 0;
 }
