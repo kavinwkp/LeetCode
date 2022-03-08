0a24ee603da30efe9dd6ed39d7b92768d7250661
kavinwkp <1817136760@qq.com>
2022-03-08 10:09:21 +0800

    3-8-1

--- C++/Primer/15/15.2/main.cpp
+++ C++/Primer/15/15.2/main.cpp
@@ -7,13 +7,13 @@ public:
     Quote() = default;
     Quote(const std::string& book, double sales_price) :
         bookNo(book), price(sales_price) {}
-    std::string isbn() const { return boolNo; }
-    virtual doble net_price(std::size_t n) const {
+    std::string isbn() const { return bookNo; }
+    virtual double net_price(std::size_t n) const {
         return n * price;
     }
     virtual ~Quote() = default;
 private:
-    std::string boolNo;
+    std::string bookNo;
 protected:
     double price = 0.0; // 普通状态下不打折的价格
 };
@@ -28,8 +28,19 @@ privete:
     double discount = 0.0;  // 以小数表示的折扣额
 };
 
+Bulk_quote::Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+                        Quote(book, p), min_qty(qty), discount(disc) {}
+
+double Bulk_quote::net_price(std::size_t cnt) const {
+    if (cnt >= min_qty) {
+        return cnt * (1 - discount) * price;
+    }
+    else return cnt * price;
+}
+
 int main() {
 
-
+    Quote q("c++primer", 12);
+    cout << q.isbn() << endl;
     return 0;
 }
